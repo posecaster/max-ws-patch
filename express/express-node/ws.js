463eddfafea72f3475932e2d37a19c4ee1a2384a
Tejaswi Gowda <tejaswi@asu.edu>
2024-02-27 17:44:09 -0700

    ac

--- express/express-node/ws.js
+++ express/express-node/ws.js
@@ -1,5 +1,6 @@
 const MaxAPI = require("max-api");
 
+
 /*
 const io = require("socket.io")();
 
@@ -24,8 +25,16 @@ io.on("connection", (socket) => {
 io.listen(4512); // trying to avoid using frequently used ports like 3000, 8000
 */
 
+function anypost(str) {
+	if (MaxAPI) {
+		MaxAPI.post(str);
+	} else {
+		console.log(str);
+	}
+}
 
 function ab2str(buf) {
+	return buf.toString();
   return String.fromCharCode.apply(null, new Uint16Array(buf));
 }
 
@@ -34,14 +43,28 @@ const wss = new WebSocket.Server({ port: 44444 });
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-var data = ab2str(message);
-		//console.log(JSON.parse(data));
-    		MaxAPI.outlet(JSON.parse(data));
+        //console.log('received: %s', message);
+		var data = ab2str(message);
+		anypost(JSON.parse(data).image);
+		//anypost(Object.keys(JSON.parse(data)));
+		
+
+		//const img = new ImageData(JSON.parse(data).image);
+
+
+    	MaxAPI.outlet(JSON.parse(data));
 
     });
     
     //ws.send('something');
-    }
-);
+});
 
+
+
+
+const OSC = require('osc-js')
+
+const config = { udpClient: { port: 9129 } }
+const osc = new OSC({ plugin: new OSC.BridgePlugin(config) })
+
+osc.open() 
